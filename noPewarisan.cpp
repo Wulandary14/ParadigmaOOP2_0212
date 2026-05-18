238e7c0e97cd9a7fb833b1cd561a32e660157a5c
Wulandary14 <Moonywulan14@gmail.com>
2026-05-18 18:47:14 +0700

    commit 20

---
+++ noPewarisan.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final{
+public:
+    virtual void perkenalan() {
+        cout << "Halo saya Function dari base class";
+    }
+};
+class baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "Halo saya Function dari base class";
+    }
+};
