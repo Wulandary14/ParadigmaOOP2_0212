55b918223f4905dc34186b64f56c9cff8d8cd15a
Wulandary14 <Moonywulan14@gmail.com>
2026-05-18 18:12:18 +0700

    commit 3

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -19,5 +19,16 @@ public:
 class lia :public seseorang {
 public:
     void pesan() {
+        cout << "Pesan dari lia" << endl;
     }
+};
+
+int main() {
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
 }
